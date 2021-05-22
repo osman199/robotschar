f733e5e55bdcc1245f6d9532d014aff2e86234fb
Osman Osman <osma@student.chalmers.se>
2021-05-22 20:48:57 +0200

    Updates

---
+++ precache-manifest.37543e81ac31a9eaa6c9e210b9d63cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "036445f6f61500760117",
+    "url": "/robotschar/static/css/main.bd3418d4.chunk.css"
+  },
+  {
+    "revision": "036445f6f61500760117",
+    "url": "/robotschar/static/js/main.075b571a.chunk.js"
+  },
+  {
+    "revision": "67d689f76c9c6afe4192",
+    "url": "/robotschar/static/js/runtime~main.43aff75a.js"
+  },
+  {
+    "revision": "946e1a81f7f1604f8ccb",
+    "url": "/robotschar/static/css/2.ffb5e771.chunk.css"
+  },
+  {
+    "revision": "946e1a81f7f1604f8ccb",
+    "url": "/robotschar/static/js/2.a2d949c5.chunk.js"
+  },
+  {
+    "revision": "f2c121df5bc05f03f3ea",
+    "url": "/robotschar/static/js/3.b1248334.chunk.js"
+  },
+  {
+    "revision": "0178adbd88ced1b5236313110c43aa7f",
+    "url": "/robotschar/index.html"
+  }
+];
